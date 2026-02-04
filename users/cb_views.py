4325fed91e45f3c0c7fd1b40a8b37abac551d9b2
sangbo <tkdqh57@naver.com>
2026-02-04 19:09:14 +0900

    feat: chapter_08 과제 업로드

--- users/cb_views.py
+++ users/cb_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model, login
 from django.core import signing
 from django.core.signing import TimestampSigner, SignatureExpired
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView
 
@@ -12,27 +12,27 @@ from utils.email import send_email
 User = get_user_model()
 
 class SignupView(CreateView):
-    template_name = 'registration/signup.html'
+    template_name = "registration/signup.html"
     form_class = SignupForm
 
     def form_valid(self, form):
         user = form.save()
-        #인증 메일 발송
+        # 인증 메일 발송
         signer = TimestampSigner()
         signed_user_email = signer.sign(user.email)
         signer_dump = signing.dumps(signed_user_email)
 
-        url = f"{self.request.scheme}://{self.request.META["HTTP_HOST"]}/users/verify/{signer_dump}"
+        url = f'{self.request.scheme}://{self.request.META["HTTP_HOST"]}/users/verify/?code={signer_dump}'
         subject = f"[Todo]{user.email}님의 이메일 인증 링크입니다."
         message = f"""
-            아래의 링크를 클릭하여 이메일 인증을 완료해주세요. \n\n
+            아래의 링크를 클릭하여 이메일 인증을 완료해주세요.\n\n
             {url}
             """
         send_email(subject=subject, message=message, from_email=None, to_email=user.email)
 
         return render(
             request=self.request,
-            template_name='registration/signup_done.html',
+            template_name="registration/signup_done.html",
             context={
                 'user': user,
             }
@@ -46,7 +46,12 @@ def verify_email(request):
         decoded_user_email = signing.loads(code)
         user_email = signer.unsign(decoded_user_email, max_age=60 * 5)
     except (TypeError, SignatureExpired):
-        return render(request, 'registration/verify_success.html')
+        return render(request, 'registration/verify_failed.html')
+
+    user = get_object_or_404(User, email=user_email)
+    user.is_active = True
+    user.save()
+    return render(request, 'registration/verify_success.html')
 
 class LoginView(FormView):
     template_name = "registration/login.html"
